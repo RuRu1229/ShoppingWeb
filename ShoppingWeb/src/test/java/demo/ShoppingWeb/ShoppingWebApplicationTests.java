9c6709c20581d1f7bc15d465b92a6a4892b3e5a0
RuRu1229 <85883409+RuRu1229@users.noreply.github.com>
2024-10-19 23:42:50 +0800

    Shopping

---
+++ ShoppingWeb/src/test/java/demo/ShoppingWeb/ShoppingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ShoppingWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
